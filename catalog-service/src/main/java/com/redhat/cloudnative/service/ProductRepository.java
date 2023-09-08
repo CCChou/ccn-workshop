7a95ae375d1dd7d91b2fb2e22681455f0feb22b8
molasun <mola79321@gmail.com>
2023-09-08 11:20:03 +0800

    Update order-service, catalog-service, payment-service

--- catalog-service/src/main/java/com/redhat/cloudnative/service/ProductRepository.java
+++ catalog-service/src/main/java/com/redhat/cloudnative/service/ProductRepository.java
@@ -11,25 +11,26 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class ProductRepository {
 
-    //TODO: Autowire the jdbcTemplate here
+    // TODO: Autowire the jdbcTemplate here
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    //TODO: Add row mapper here
+    // TODO: Add row mapper here
     private RowMapper<Product> rowMapper = (rs, rowNum) -> new Product(
             rs.getString("itemId"),
             rs.getString("name"),
             rs.getString("description"),
             rs.getDouble("price"));
 
-    //TODO: Create a method for returning all products
+    // TODO: Create a method for returning all products
     public List<Product> readAll() {
         return jdbcTemplate.query("SELECT * FROM catalog", rowMapper);
     }
 
-    //TODO: Create a method for returning one product
+    // TODO: Create a method for returning one product
     public Product findById(String id) {
-        return this.jdbcTemplate.queryForObject("SELECT * FROM catalog WHERE itemId = ?", new Object[]{id}, rowMapper);
+        return this.jdbcTemplate.queryForObject("SELECT * FROM catalog WHERE itemId = ?", rowMapper,
+                new Object[] { id });
     }
 
 }
